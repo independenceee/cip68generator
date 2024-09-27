d3f5ac9c5187af5275d2bb16cf97d83facc23561
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2024-09-27 16:40:59 +0700

    develop: offchain code with mint nft cip68 contract

---
+++ src/interfaces/cip68.interface.ts
@@ -0,0 +1,6 @@
+export interface ICip68Contract {
+    // mint: () => Promise<string>;
+    // burn: () => Promise<string>;
+    // update: () => Promise<string>;
+    // remove: () => Promise<string>;
+}
--- tests/cip68.test.ts
+++ tests/cip68.test.ts
@@ -1,10 +1,62 @@
-import { describe, test, expect } from "@jest/globals";
-import { BlockfrostProvider, metadataToCip68 } from "@meshsdk/core";
+import { apiKey, apiUrl } from "../src/configs";
+import { describe, test, expect, beforeEach } from "@jest/globals";
+import {
+    BlockfrostProvider,
+    BrowserWallet,
+    KoiosProvider,
+    MeshTxBuilder,
+    MeshWallet,
+    metadataToCip68,
+} from "@meshsdk/core";
+import { Cip68Contract } from "../src";
 
-describe("Mint contract with cip68", function () {
-    
+describe("Mint, Burn, Update, Remove Assets (NFT/TOKEN) CIP68", function () {
+    let meshTxBuilder: MeshTxBuilder;
+    let wallet: BrowserWallet | MeshWallet;
+    let blockfrostProvider: BlockfrostProvider | KoiosProvider;
 
-    test("Mint Contract", function () {
+    beforeEach(async function () {
+        blockfrostProvider = new BlockfrostProvider("preprodHXZNMTECARQ3jlUE0RvCBT2qOK6JRtQf");
+        wallet = new MeshWallet({
+            networkId: 0,
+            fetcher: blockfrostProvider,
+            submitter: blockfrostProvider,
+            key: {
+                type: "root",
+                bech32: "xprv16zlhjxs29l9zk0aaf54ttn32nsrl9l855yqpsurnwjxfu2kd93dc4xx0pvxf0ffhzl9vc9vpcqsmmhhfu3c8nfusdj0yh8mg2kzgr797vxrtut4czgwjj4pdzfnstcwy6n0jfjw6tyeuqxdynl8msnu3cv8j5msy",
+            },
+        });
 
+        meshTxBuilder = new MeshTxBuilder({
+            fetcher: blockfrostProvider,
+            evaluator: blockfrostProvider,
+            submitter: blockfrostProvider,
+        });
+    });
+
+    test("Mint", async function () {
+        const cip68Contract = new Cip68Contract({
+            fetcher: blockfrostProvider,
+            wallet: wallet,
+            meshTxBuilder: meshTxBuilder,
+        });
+
+        const unsignedTx = await cip68Contract.mint("Nguyen Duy Khanh", "1", {
+            name: "ERC721",
+            image: "ipfs://QmRzicpReutwCkM6aotuKjErFCUD213DpwPq6ByuzMJaua",
+            mediaType: "image/jpg",
+            description: "Blockchain Developer",
+        });
+        const signedTx = await wallet.signTx(unsignedTx, true);
+        const txHash = await wallet.submitTx(signedTx);
+        expect(txHash.length).toBe(64);
+    });
+
+    test("Burn", async function () {
+        const cip68Contract = new Cip68Contract({
+            fetcher: blockfrostProvider,
+            wallet: wallet,
+            meshTxBuilder: meshTxBuilder,
+        });
     });
 });
